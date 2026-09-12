7cc6741572f8281e2340a2af3b0fd7ecb5873392
lzxuniverse <lzx.infinitiverse@gmail.com>
2026-09-12 19:47:40 +0700

    update

--- resources/views/properties/index.blade.php
+++ resources/views/properties/index.blade.php
@@ -417,7 +417,7 @@
                     <!-- Search Icon SVG -->
                     <svg class="search-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                    </</svg>
+                    </svg>
                     <input type="text" name="search" class="search-input" placeholder="Search by name, city or country..." value="{{ request('search') }}">
                 </div>
                 <button type="submit" class="btn">Search</button>
